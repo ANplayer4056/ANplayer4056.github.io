f04eb2b14e56f796e258e47ddc90eb8508277d2c
gior4056 <gior4056@gmail.com>
2019-05-29 16:04:46 +0800

    tst

---
+++ tstAA/js/config.js
@@ -0,0 +1,6 @@
+var Config = function () {
+    this.ApiUrl = "http://apitst.icrown.tw/";
+    this.ImageUrl = "http://gametst.icrown.tw/";
+    this.AuthorizedKey = "e34faabeda4141efa49952eb633684e4";
+
+}; 
